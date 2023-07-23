e703d019985a617849c9157988d932059aa9eedf
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-24 01:43:53 +0900

    connect to mongodb and create the server

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const bodyParser = require('body-parser');
+const route = require('./routes/videoRoute.js');
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => console.log(error));
+database.once('connected', () => console.log('Connected to database'));
+
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', route);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+});
