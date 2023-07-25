4aebee9b5d95e6354d5d03aea08851512dfc1248
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-25 20:46:29 +0900

    Update videoServices.js
    list of service made:
    -getVideoListService
    -getProductListService
    -getCommentListService
    -addCommentService

--- services/videoServices.js
+++ services/videoServices.js
@@ -1,16 +1,39 @@
-// make video service
-
-// Path: tokopediaplay/services/videoServices.js
 const Video = require('../models/videoModel.js');
 
+async function getVideoListService() {
+    try {
+        return await Video.find().select('urlImageThumbnail');
+    } catch (error) {
+        throw new Error(error.message);
+    };
+};
 
-// get id, urlImage, thumbnail
-function getVideoList() {
-     try {
-        return Video.find().select('urlImage thumbnail');
+
+// get all array of object product list based on vide id
+async function getProductListService(videoId) {
+    try {
+        return await Video.find({ _id: videoId }).select({ productList: 1 });
     } catch (error) {
         throw new Error(error.message);
     }
-}
+};
 
-module.exports = { getVideoList };
+// get all array of object comment list based on vide id
+async function getCommentListService(videoId) {
+    try {
+        return await Video.find({ _id: videoId }).select({ commentList: 1 });
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+// add comment to comment list based on video id
+async function addCommentService(videoId, name, comment) {
+    try {
+        return await Video.updateOne({ _id: videoId }, { $push: { commentList: { name: name, comment: comment } } });
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+module.exports = { getVideoListService, getProductListService, getCommentListService, addCommentService };
