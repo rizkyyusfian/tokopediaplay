21e1bd282c8660c47c40815ef21af140e2201c96
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-25 20:47:17 +0900

    Update videoRoute.js

--- routes/videoRoute.js
+++ routes/videoRoute.js
@@ -4,4 +4,10 @@ const videoController = require('../controllers/videoController.js');
 
 router.get('/', videoController.getVideo);
 
+router.get('/product/:videoId', videoController.getProduct);
+
+router.get('/comment/:videoId', videoController.getComment);
+
+router.post('/comment/:videoId', videoController.addComment);
+
 module.exports = router;
