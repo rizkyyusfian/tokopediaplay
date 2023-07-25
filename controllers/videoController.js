51e4175301174238a2970bc5aa7e5b507fc80250
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-26 01:09:02 +0900

    update error message send to client

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,7 +7,7 @@ async function getVideo(req, res) {
         const videoList = await getVideoListService();
         res.status(200).json(videoList);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json("Failed to get video list, Error Message : " + error.message);
     }
 };
 
@@ -18,7 +18,7 @@ async function getProduct(req, res) {
         const productList = await getProductListService(videoId);
         res.status(200).json(productList);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json("Failed to get product list, Error Message : " + error.message);
     }
 };
 
@@ -29,7 +29,7 @@ async function getComment(req, res) {
         const commentList = await getCommentListService(videoId);
         res.status(200).json(commentList);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json("Failed to get comment list, Error Message : " + error.message);
     }
 };
 
@@ -37,11 +37,11 @@ async function getComment(req, res) {
 async function addComment(req, res) {
     try {
         const videoId = req.params.videoId;
-        const { name, comment } = req.body;
-        const commentList = await addCommentService(videoId, name, comment);
-        res.status(201).json("Comment Added");
+        const { userName, comment } = req.body;
+        await addCommentService(videoId, userName, comment);
+        res.status(201).json("Comment Successfully Added");
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json("Failed to add comment, Error Message : " + error.message);
     }
 };
 
