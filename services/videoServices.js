d7b828af74d62dd45f17316e3567f24fe853b3b4
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-26 01:10:23 +0900

    Update videoServices.js
    -update naming on some parameter and variabel
    -fixing query to send to client

--- services/videoServices.js
+++ services/videoServices.js
@@ -3,7 +3,7 @@ const Video = require('../models/videoModel.js');
 // Get all video list
 async function getVideoListService() {
     try {
-        return await Video.find().select('urlImageThumbnail');
+        return await Video.find().select('ImageThumbnailUrl');
     } catch (error) {
         throw new Error(error.message);
     };
@@ -13,7 +13,7 @@ async function getVideoListService() {
 // Get all product list based on video id
 async function getProductListService(videoId) {
     try {
-        return await Video.find({ _id: videoId }).select({ productList: 1 });
+        return await Video.find({ _id: videoId }).select({ "productList._id": 1, "productList.name": 1, "productList.price": 1, "productList.link": 1, "_id": 0 });
     } catch (error) {
         throw new Error(error.message);
     }
@@ -22,16 +22,16 @@ async function getProductListService(videoId) {
 // Get all comment list based on video id
 async function getCommentListService(videoId) {
     try {
-        return await Video.find({ _id: videoId }).select({ commentList: 1 });
+        return await Video.find({ _id: videoId }).select({ "commentList.userName": 1, "commentList.comment": 1, "commentList.createdAt": 1, "commentList.updatedAt": 1, "_id": 0  });
     } catch (error) {
         throw new Error(error.message);
     }
 };
 
 // Add comment based on video id
-async function addCommentService(videoId, name, comment) {
+async function addCommentService(videoId, userName, comment) {
     try {
-        return await Video.updateOne({ _id: videoId }, { $push: { commentList: { name: name, comment: comment } } });
+        return await Video.updateOne({ _id: videoId }, { $push: { commentList: { userName: userName, comment: comment } } });
     } catch (error) {
         throw new Error(error.message);
     }
