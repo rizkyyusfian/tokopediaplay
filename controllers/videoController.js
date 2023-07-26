7be39f724fa3b2c9df9ea248b4beccf82de76b8c
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-26 10:22:58 +0900

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,7 +7,7 @@ async function getVideo(req, res) {
         const videoList = await getVideoListService();
         res.status(200).json({"videoList": videoList});
     } catch (error) {
-        res.status(500).json("Failed to get video list, Error Message : " + error.message);
+        res.status(500).json("Failed to get video list, Error Message: " + error.message);
     }
 };
 
@@ -18,7 +18,7 @@ async function getProduct(req, res) {
         const productList = await getProductListService(videoId);
         res.status(200).json(productList);
     } catch (error) {
-        res.status(500).json("Failed to get product list, Error Message : " + error.message);
+        res.status(500).json("Failed to get product list, Error Message: " + error.message);
     }
 };
 
@@ -29,7 +29,7 @@ async function getComment(req, res) {
         const commentList = await getCommentListService(videoId);
         res.status(200).json(commentList);
     } catch (error) {
-        res.status(500).json("Failed to get comment list, Error Message : " + error.message);
+        res.status(500).json("Failed to get comment list, Error Message: " + error.message);
     }
 };
 
@@ -41,7 +41,7 @@ async function addComment(req, res) {
         await addCommentService(videoId, userName, comment);
         res.status(201).json("Comment Successfully Added");
     } catch (error) {
-        res.status(400).json("Failed to add comment, Error Message : " + error.message);
+        res.status(400).json("Failed to a add comment, Error Message: " + error.message);
     }
 };
 
